6c2c15b7adb7d84b774d501309d4ef07562aea9b
soochan <tncksdl05@gmail.com>
2023-11-29 23:28:14 +0900

    refactor(BaseBallGame): 재입력은 요구사항에 없었음으로 삭제

--- src/main/java/baseball/controller/BaseBallGameController.java
+++ src/main/java/baseball/controller/BaseBallGameController.java
@@ -1,10 +1,6 @@
 package baseball.controller;
 
 import java.util.List;
-import java.util.function.BiConsumer;
-import java.util.function.BiFunction;
-import java.util.function.Function;
-import java.util.function.Supplier;
 import baseball.model.BaseBallGame;
 import baseball.model.BaseBallNumbers;
 import baseball.model.BaseBallResult;
@@ -33,11 +29,11 @@ public class BaseBallGameController {
         BaseBallNumbers randomBaseBallNumbers = BaseBallNumbers.create(numberGenerator);
         BaseBallGame baseBallGame = BaseBallGame.of(randomBaseBallNumbers);
         while (baseBallGame.isInProgress()) {
-            BaseBallNumbers userBaseBallNumbers = fetch(this::readBaseBallNumbers);
+            BaseBallNumbers userBaseBallNumbers = readBaseBallNumbers();
             BaseBallResult result = baseBallGame.play(userBaseBallNumbers);
             outputView.printResult(result);
         }
-        if (fetch(this::readRestart).isRestart()) {
+        if (readRestart().isRestart()) {
             playingGame();
         }
     }
@@ -52,53 +48,4 @@ public class BaseBallGameController {
         return BaseBallNumbers.from(baseBallNumbers);
     }
 
-    /**
-     * 매개변수 X, 반환값 O
-     * <p>
-     * [예시] 1. view부터 dto을 받을때 2. dto을 domain으로 변환할때
-     */
-    private <T> T fetch(Supplier<T> supplier) {
-        try {
-            return supplier.get();
-        } catch (IllegalArgumentException e) {
-            outputView.printExceptionMessage(e.getMessage());
-            return fetch(supplier);
-        }
-    }
-
-    /**
-     * 매개변수 T, 반환값 R
-     */
-    private <T, R> R fetch(Function<T, R> function, T input) {
-        try {
-            return function.apply(input);
-        } catch (IllegalArgumentException e) {
-            outputView.printExceptionMessage(e.getMessage());
-            return fetch(function, input);
-        }
-    }
-
-    /**
-     * 매개변수 T U, 반환값 R
-     */
-    private <T, U, R> R fetch(BiFunction<T, U, R> function, T t, U u) {
-        try {
-            return function.apply(t, u);
-        } catch (IllegalArgumentException e) {
-            outputView.printExceptionMessage(e.getMessage());
-            return fetch(function, t, u);
-        }
-    }
-
-    /**
-     * 매개변수 T U, 반환값 X
-     */
-    private <T, U> void fetch(BiConsumer<T, U> function, T t, U u) {
-        try {
-            function.accept(t, u);
-        } catch (IllegalArgumentException e) {
-            outputView.printExceptionMessage(e.getMessage());
-            fetch(function, t, u);
-        }
-    }
 }
